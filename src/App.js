87fe536c5ac128637238e73d3e8c49dccf83a055
Apiwit22 <159275649+Apiwit22@users.noreply.github.com>
2024-03-09 22:45:48 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Nav from "./component/Nav";
+import Homepage from "./component/Homepage/Home";
+import LoginPage from "./component/Loginpage/Login";
+import ManUpage from "./component/ManUpage/Man_U"
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+
+      <BrowserRouter>
+        <Switch>
+            <Route exact path='/'>
+                  <Homepage />
+            </Route>
+          
+            <Route path='/login'>
+                  <LoginPage />
+            </Route>
+
+            <Route exact path='/Man_United'>
+                  <ManUpage />
+            </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
